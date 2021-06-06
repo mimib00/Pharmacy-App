eb1ac6f53df17a45cecabf24022fcdcd2d9516a7
Melkia Mohamed <83797689+mimib00@users.noreply.github.com>
2021-06-06 16:21:06 +0100

    added a custom clipper

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:pharmacy_app/utils/color.dart';
+import 'package:pharmacy_app/widgets/backgroundClipper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Home'),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: Stack(
+          children: [
+            //Background
+            ClipPath(
+              clipper: BackgroundClipper(),
+              child: Container(
+                height: size.height * 0.75,
+                decoration: BoxDecoration(color: kPrimaryColor),
+              ),
+            )
+            //Rest
+          ],
+        ),
       ),
     );
   }
